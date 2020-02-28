73a69e7d7b4640dee9e73d74c63bae55949924d1
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-02-28 22:07:44 +0530

    Change

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let submit = $('register');
+// $("#register").click(function () {
+    // $('#username').empty();
+    // $('#email').empty();
+    // $('#number').empty();
+    // $('#age').empty();
+    // $('#password').empty();
+// })
